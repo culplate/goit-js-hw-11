296d79a33193e6b6575d919d821203a3cae8ab86
culplate <culplate@gmail.com>
2023-12-16 15:52:03 +0100

    Data fetching and rendering

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 import axios from "axios";
 import Notiflix from "notiflix";
 const API_KEY = '41284916-bd469a2634b9bca975146e6bc';
+const BASE_URL = 'https://pixabay.com/api'
+
+const refs = {
+    searchBtn: document.querySelector('.search-form button'),
+    searchForm: document.querySelector('.search-form'),
+    searchInput: document.querySelector('.search-form input'),
+    gallery: document.querySelector('.gallery')
+}
+
+refs.searchBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const formData = new FormData(refs.searchForm);
+    const { searchQuery } = Object.fromEntries(formData.entries());
+    console.log(searchQuery)
+    if (searchQuery.length === 0) {
+        Notiflix.Notify.warning('Your request should not be empty')
+        return;
+    }
+    renderData(searchQuery);
+})
+
+function createMarkup(arr) {
+    return arr.map(({ webformatURL }) => {
+        return `
+            <div class="photo-card">
+                <img src="${webformatURL}" alt="" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                    </p>
+                </div>
+            </div>
+        `
+    }
+    ).join('')
+}
+
+async function renderData(query) {
+    try {
+        const data = await fetchData(query);
+        refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchData(query) {
+    try {
+        const response = await axios(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                per_page: 40,
+                page: 1
+            }
+        })
+        return response.data.hits;
+    } catch (error) {
+        Notiflix.Notify.failure(error.response.data)
+    }
+}
